ab02bd1f46db5c128c2843fd8ed94576299c5376
kai-gangi <kaigangi72@gmail.com>
2024-12-30 19:01:25 -0800

    Initial Commit

---
+++ RESET30/RESET30App.swift
@@ -0,0 +1,17 @@
+//
+//  RESET30App.swift
+//  RESET30
+//
+//  Created by Kai Gangi on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RESET30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
